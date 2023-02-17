0ceca7ca1b7e7b6ac5c692141c41687a1eff27a8
Tiago <tnhhilario@gmail.com>
2023-02-17 00:46:41 -0300

    Implementado o id para manipulação de nomes

--- app/src/main/java/com/tiago/agenda/dao/AlunoDao.java
+++ app/src/main/java/com/tiago/agenda/dao/AlunoDao.java
@@ -6,11 +6,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AlunoDao {
+    private final static List<Aluno> alunos = new ArrayList<>(); // simulação de um banco de dados
+    private static int contadorIds = 1;
 
-    private  final  static List<Aluno> alunos = new ArrayList<>(); // simulação de um banco de dados
     public void salva(Aluno aluno) {
+        aluno.setId(contadorIds);
         alunos.add(aluno);
-    } //
+        contadorIds++;
+    }
+
+    public void editar(Aluno aluno) {
+        Aluno alunoEncontrado = null;
+        for (Aluno a : alunos) {
+            if (a.getId() == aluno.getId()) {
+                alunoEncontrado = a;
+            }
+            if (alunoEncontrado != null) {
+                int posicaoAlunoEncontrado = alunos.indexOf(alunoEncontrado);
+                alunos.set(posicaoAlunoEncontrado, aluno);
+            }
+        }
+    }
+
+
+
 
     public List<Aluno> todos() {
         return new ArrayList<>(alunos);
